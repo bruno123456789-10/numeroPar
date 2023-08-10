a8e75bd818cc2b472dac4bb5f8099d1cc9694173
bruno123456789-10 <brunothome26@gmail.com>
2023-08-10 09:51:56 -0300

    Adicionar arquivos de projeto.

---
+++ numeroPar/Controller/CalcularPar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroPar.Controller
+{
+    internal class CalcularPar
+    {
+        public string Calcular(int valor)
+        {
+            string ju = "";
+
+            if (valor % 2 == 0)
+            {
+                
+            }
+
+            return ju;
+        }
+    }
+}
---
+++ numeroPar/View/TelaNumeroPar.cs
@@ -0,0 +1,34 @@
+﻿using numeroPar.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numeroPar.View
+{
+    public partial class TelaNumeroPar : Form
+    {
+        public TelaNumeroPar()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(textBox1.Text);
+
+            if (numero % 2 == 0 ) { 
+                label2.Text = "Numero Par";
+            }
+            else
+            {
+                label2.Text = "Numero Impar";
+            }
+        }
+    }
+}
